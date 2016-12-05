b9ec8c7b3bf403312687ba4600d297791ff721e6
pro-to-tip <nenjoi@yahoo.com>
2016-12-05 13:28:36 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ return [
 	// !!! change secret key
 	'secret' => 'u65hb5nsd3io0e90ie9p0rzhj4yhqujh',
 	'users' => [
-		'Try' => [
+		'Admin' => [
 			'name' => 'Try Try',
 			// !!! change your password
-			'password' => 'Try'
+			'password' => 'Admin'
 		]
 		
 		
